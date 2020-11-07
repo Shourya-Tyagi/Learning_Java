cf33b62c2c922b6fccbe6bbd2031ca629b548570
Shourya Tyagi <63458985+Shourya-Tyagi@users.noreply.github.com>
2020-11-07 16:53:42 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,16 @@
+public class first{
+  public static void main(String[] args) {
+    final double G = 9.8 ;
+    int aa = (int)G  ; // cast process
+    int bb = (int)Math.round(G) ;
+    System.out.println(aa+bb);
+    int x = 3 ;
+    int y = 4 ;
+    double c = Math.pow(x,y) ;
+    System.out.println(c);
+
+
+
+
+  }
+}
---
+++ userinput.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class userinput {
+  public static void main(String[] args) {
+    Scanner myobj = new Scanner(System.in) ; // created a scanner object
+    System.out.println("enter username1:");
+    int user_1 = myobj.nextInt() ;
+    System.out.println("enter username2:");
+    int user_2 = myobj.nextInt() ;
+    System.out.println("username1 is " + user_1);
+    System.out.println("username2 is " + user_2);
+
+
+
+
+
+
+  }
+
+}
